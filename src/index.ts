bd05e1ab815687623d07b1e976e4562fb8987fcc
Sean.Rennie <rnnsea001@gmail.com>
2021-04-20 18:51:07 +0100

    add: textures and first rondavel

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,86 @@ const gui = new dat.GUI();
 
 // Canvas
 const canvas: HTMLCanvasElement | null = document.querySelector('canvas.webgl');
+if (!canvas)
+  throw Error(
+    'Could not find selected canvas in the document. Did you use the correct selector?',
+  );
+
+// Textures
+const textureLoader = new THREE.TextureLoader();
+
+// door
+const doorAlpha = textureLoader.load('textures/door/alpha.jpg');
+const doorAmbientOcclusion = textureLoader.load(
+  'textures/door/ambientOcclusion.jpg',
+);
+const doorColor = textureLoader.load('textures/door/color.jpg');
+const doorHeight = textureLoader.load('textures/door/height.jpg');
+const doorMetalness = textureLoader.load('textures/door/metalness.jpg');
+const doorNormal = textureLoader.load('textures/door/normal.jpg');
+const doorRoughness = textureLoader.load('textures/door/roughness.jpg');
+
+// thatch
+const thatchAmbientOcclusion = textureLoader.load(
+  'textures/thatch/ambientOcclusion.jpg',
+);
+thatchAmbientOcclusion.repeat.set(5, 1);
+thatchAmbientOcclusion.wrapS = THREE.RepeatWrapping;
+thatchAmbientOcclusion.wrapT = THREE.RepeatWrapping;
+const thatchColor = textureLoader.load('textures/thatch/colorDark.jpg');
+thatchColor.repeat.set(5, 1);
+thatchColor.wrapS = THREE.RepeatWrapping;
+thatchColor.wrapT = THREE.RepeatWrapping;
+const thatchHeight = textureLoader.load('textures/thatch/displacement.jpg');
+thatchHeight.repeat.set(5, 1);
+thatchHeight.wrapS = THREE.RepeatWrapping;
+thatchHeight.wrapT = THREE.RepeatWrapping;
+const thatchMetalness = textureLoader.load('textures/thatch/metalness.jpg');
+thatchMetalness.repeat.set(5, 1);
+thatchMetalness.wrapS = THREE.RepeatWrapping;
+thatchMetalness.wrapT = THREE.RepeatWrapping;
+const thatchNormal = textureLoader.load('textures/thatch/normal.jpg');
+thatchNormal.repeat.set(5, 1);
+thatchNormal.wrapS = THREE.RepeatWrapping;
+thatchNormal.wrapT = THREE.RepeatWrapping;
+const thatchRoughness = textureLoader.load('textures/thatch/roughness.jpg');
+thatchRoughness.repeat.set(5, 1);
+thatchRoughness.wrapS = THREE.RepeatWrapping;
+thatchRoughness.wrapT = THREE.RepeatWrapping;
+
+// mud
+const mudAmbientOcclusion = textureLoader.load(
+  'textures/mud/ambientOcclusion.jpg',
+);
+mudAmbientOcclusion.repeat.set(5, 1);
+mudAmbientOcclusion.wrapS = THREE.RepeatWrapping;
+mudAmbientOcclusion.wrapT = THREE.RepeatWrapping;
+const mudColor = textureLoader.load('textures/mud/color.jpg');
+mudColor.repeat.set(5, 1);
+gui.add(mudColor.repeat, 'x').min(0).max(10).step(1);
+gui.add(mudColor.repeat, 'y').min(0).max(10).step(1);
+mudColor.wrapS = THREE.RepeatWrapping;
+mudColor.wrapT = THREE.RepeatWrapping;
+const mudHeight = textureLoader.load('textures/mud/displacement.jpg');
+mudHeight.repeat.set(5, 1);
+mudHeight.wrapS = THREE.RepeatWrapping;
+mudHeight.wrapT = THREE.RepeatWrapping;
+const mudMetalness = textureLoader.load('textures/mud/metalness.jpg');
+mudMetalness.repeat.set(5, 1);
+mudMetalness.wrapS = THREE.RepeatWrapping;
+mudMetalness.wrapT = THREE.RepeatWrapping;
+const mudNormal = textureLoader.load('textures/mud/normal.jpg');
+mudNormal.repeat.set(5, 1);
+mudNormal.wrapS = THREE.RepeatWrapping;
+mudNormal.wrapT = THREE.RepeatWrapping;
+const mudRoughness = textureLoader.load('textures/mud/roughness.jpg');
+mudRoughness.repeat.set(5, 1);
+mudRoughness.wrapS = THREE.RepeatWrapping;
+mudRoughness.wrapT = THREE.RepeatWrapping;
+const mudBump = textureLoader.load('textures/mud/bump.jpg');
+mudBump.repeat.set(5, 1);
+mudBump.wrapS = THREE.RepeatWrapping;
+mudBump.wrapT = THREE.RepeatWrapping;
 
 // Scene
 const scene = new THREE.Scene();
@@ -27,34 +107,91 @@ const rondavelSizes = {
   windowHeight: 1,
   windowWidth: 2,
   roofHeight: 2,
-  roofRadius: 2.65,
+  roofRadius: 2.75,
+  roofBasePosition: 3.1,
+  doorTheta: 0.8,
 };
 
 const rondavel = new THREE.Group();
 scene.add(rondavel);
 
-const bodyMaterial = new THREE.MeshStandardMaterial({ color: 'brown' });
-const bodyGeometry = new THREE.CylinderGeometry(
+// Walls/body
+const wallMaterial = new THREE.MeshStandardMaterial({
+  color: 0xd3d3d3,
+  map: mudColor,
+  aoMap: mudAmbientOcclusion,
+  normalMap: mudNormal,
+  displacementMap: mudHeight,
+  displacementScale: 0.01,
+  roughnessMap: mudRoughness,
+  metalnessMap: mudMetalness,
+  bumpMap: mudBump,
+  // wireframe: true,
+});
+const wallGeometry = new THREE.CylinderGeometry(
   rondavelSizes.radius,
   rondavelSizes.radius,
   rondavelSizes.height,
   100,
+  100,
+  true,
 );
-const bodyMesh = new THREE.Mesh(bodyGeometry, bodyMaterial);
+const wall = new THREE.Mesh(wallGeometry, wallMaterial);
+wall.position.set(0, rondavelSizes.height / 2, 0);
 
-const roofMaterial = new THREE.MeshStandardMaterial({ color: 'yelow' });
+// Roof
+const roofMaterial = new THREE.MeshStandardMaterial({
+  map: thatchColor,
+  aoMap: thatchAmbientOcclusion,
+  normalMap: thatchNormal,
+  displacementMap: thatchHeight,
+  displacementScale: 0.01,
+  roughnessMap: thatchRoughness,
+  metalnessMap: thatchMetalness,
+  // wireframe: true,
+});
 const roofGeometry = new THREE.CylinderGeometry(
   0,
   rondavelSizes.roofRadius,
   rondavelSizes.roofHeight,
-  100,
+  250,
+  250,
 );
 
-const roofMesh = new THREE.Mesh(roofGeometry, roofMaterial);
+const roof = new THREE.Mesh(roofGeometry, roofMaterial);
+roof.position.set(0, rondavelSizes.roofBasePosition, 0);
+// roof.geometry.parameters.openEnded = true;
 
-rondavel.add(roofMesh);
+// Door
+const doorMaterial = new THREE.MeshStandardMaterial({
+  map: doorColor,
+  transparent: true,
+  alphaMap: doorAlpha,
+  aoMap: doorAmbientOcclusion,
+  roughnessMap: doorRoughness,
+  normalMap: doorNormal,
+  metalnessMap: doorMetalness,
+  displacementMap: doorHeight,
+  displacementScale: 0.05,
+});
 
-// Floor
+const doorGeometry = new THREE.CylinderGeometry(
+  rondavelSizes.radius + 0.001,
+  rondavelSizes.radius,
+  rondavelSizes.doorHeight,
+  100,
+  100,
+  true,
+  0,
+  rondavelSizes.doorTheta,
+);
+
+const door = new THREE.Mesh(doorGeometry, doorMaterial);
+door.position.set(0, rondavelSizes.doorHeight / 2, 0);
+
+rondavel.add(wall, roof, door);
+
+// FLOOR
 const floor = new THREE.Mesh(
   new THREE.PlaneGeometry(20, 20),
   new THREE.MeshStandardMaterial({
@@ -77,18 +214,18 @@ scene.add(floor);
  * Lights
  */
 // Ambient light
-const ambientLight = new THREE.AmbientLight('#b9d5ff', 0.2);
+const ambientLight = new THREE.AmbientLight('#b9d5ff', 0.65);
 gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001);
 scene.add(ambientLight);
 
 // Directional light
-const moonLight = new THREE.DirectionalLight('#ffffff', 0.75);
-moonLight.position.set(4, 5, -2);
-gui.add(moonLight, 'intensity').min(0).max(1).step(0.001);
-gui.add(moonLight.position, 'x').min(-5).max(5).step(0.001);
-gui.add(moonLight.position, 'y').min(-5).max(5).step(0.001);
-gui.add(moonLight.position, 'z').min(-5).max(5).step(0.001);
-scene.add(moonLight);
+const directionalLight = new THREE.DirectionalLight('#ffffff', 0.75);
+directionalLight.position.set(4, 3, 5);
+gui.add(directionalLight, 'intensity').min(0).max(1).step(0.001);
+gui.add(directionalLight.position, 'x').min(-5).max(5).step(0.001);
+gui.add(directionalLight.position, 'y').min(-5).max(5).step(0.001);
+gui.add(directionalLight.position, 'z').min(-5).max(5).step(0.001);
+scene.add(directionalLight);
 
 /**
  * Sizes
@@ -117,14 +254,14 @@ window.addEventListener('resize', () => {
  */
 // Base camera
 const camera = new THREE.PerspectiveCamera(
-  75,
+  45,
   sizes.width / sizes.height,
   0.1,
   100,
 );
 camera.position.x = 4;
-camera.position.y = 2;
-camera.position.z = 5;
+camera.position.y = 4;
+camera.position.z = 10;
 scene.add(camera);
 
 // Controls
@@ -139,16 +276,14 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
-renderer.setClearColor(0x262837);
+renderer.setClearColor(0x87cfff);
 
 /**
  * Shadows
  */
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-floor.receiveShadow = true;
 rondavel.castShadow = true;
-moonLight.castShadow = true;
 
 /**
  * Animate
