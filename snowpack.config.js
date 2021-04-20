bacb650efd785268c332ab6fa1843f6171c77a75
Sean.Rennie <rnnsea001@gmail.com>
2021-04-20 07:38:11 +0100

    snowpack starter

--- snowpack.config.js
+++ snowpack.config.js
@@ -5,13 +5,13 @@ module.exports = {
     src: { url: '/dist' },
   },
   plugins: [
-    [
-      '@snowpack/plugin-typescript',
-      {
-        /* Yarn PnP workaround: see https://www.npmjs.com/package/@snowpack/plugin-typescript */
-        ...(process.versions.pnp ? { tsc: 'yarn pnpify tsc' } : {}),
-      },
-    ],
+    // [
+    //   '@snowpack/plugin-typescript',
+    //   {
+    //     /* Yarn PnP workaround: see https://www.npmjs.com/package/@snowpack/plugin-typescript */
+    //     ...(process.versions.pnp ? { tsc: 'yarn pnpify tsc' } : {}),
+    //   },
+    // ],
   ],
   routes: [
     /* Enable an SPA Fallback in development: */
